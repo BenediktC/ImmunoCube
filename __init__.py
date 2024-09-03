2b0a45b438128f5eff7955fc0c4b9a09666625f0
benediktchristianclemens <122476674+benediktchristianclemens@users.noreply.github.com>
2024-09-03 16:59:03 +0200

    Delete __init__.py

--- __init__.py
+++
@@ -1,8 +0,0 @@
-from .FractionProfile import FractionProfile
-from .ProfileErrors import ProfileError, ProfileNegativeDistanceError, ProfileInvalidDistanceError, FractionZeroDivisionError, UnequalFractionsError, ZeroSumError, EmptyProfileListError
-from .SumProfile import SumProfile
-from .ReferenceProfile import ReferenceProfile
-from .StatProfile import StatProfile
-from .ProfilePCA import ProfilePCA
-
-__all__ = ['FractionProfile', 'ProfileError', 'ProfileNegativeDistanceError', 'ProfileInvalidDistanceError', 'FractionZeroDivisionError', 'UnequalFractionsError', 'ZeroSumError', 'EmptyProfileListError', 'SumProfile', 'ReferenceProfile', 'StatProfile', 'ProfilePCA']
